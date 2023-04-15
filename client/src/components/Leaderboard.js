4743e001d15581b082e19e062d2ba9d45d58326d
MKhour <mgkhoury2001@gmail.com>
2023-04-15 16:19:12 -0400

    fix leaderboard rendering.

--- client/src/components/Leaderboard.js
+++ client/src/components/Leaderboard.js
@@ -22,18 +22,21 @@ class Leaderboard extends Component {
       <div className="App">
         <Header />
         <h1>Leaderboard</h1>
-        <div id='tHead'>
-            <div id='silver'><p>{this.state.users[1].username}</p><p>{this.state.users[1].points}</p></div>
-            <div id='gold'><p>{this.state.users[0].username}</p><p>{this.state.users[0].points}</p></div>
-            <div id='bronze'><p>{this.state.users[2].username}</p><p>{this.state.users[2].points}</p></div>
-        </div>
-        <div id='tBody'>
-            {this.state.users.slice(3).map((user, index) => {
-                return (
-                    <div className='tRow'><p>{user.username}</p><p>{user.points}</p></div>
-                )
-            })}
-        </div>
+        {(this.state.users.length > 0) &&
+          <><div id='tHead'>
+              <div id='silver'><p>{this.state.users[1].username}</p><p>{this.state.users[1].points}</p></div>
+              <div id='gold'><p>{this.state.users[0].username}</p><p>{this.state.users[0].points}</p></div>
+              <div id='bronze'><p>{this.state.users[2].username}</p><p>{this.state.users[2].points}</p></div>
+          </div>
+          <div id='tBody'>
+              {this.state.users.slice(3).map((user, index) => {
+                  return (
+                      <div className='tRow'><p>{user.username}</p><p>{user.points}</p></div>
+                  )
+              })}
+          </div>
+          </>
+        }
       </div>
     );
   }
