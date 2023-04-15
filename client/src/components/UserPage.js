c87f40369eaa1ef9d4004f01c3e2b3e622ddde2e
MKhour <mgkhoury2001@gmail.com>
2023-04-15 16:08:12 -0400

    built out UI elements on user page.

--- client/src/components/UserPage.js
+++ client/src/components/UserPage.js
@@ -27,29 +27,25 @@ class UserPage extends Component {
     render() {
       return (
         <div className="App">
-          <Header />
-           {/* header */}
-          <div>
-            <h1>{this.state.user == null? "placeholder" : this.state.user.username}</h1>
+            <Header />
+            {/* header */}
             <div>
-                <p>Your Points: {this.state.user == null? "placeholder" : this.state.user.points}</p>
-            </div>
-            <div>
-                <p>Your Achievements:</p>
-                <div>{this.state.achievements.map((ach, index) => {
-                    return (
-                        <div>
-                            <img src={require('../imgs/' + ach.icon_link)} width="100px"/>
-                            <p>{ach.achievement_name}</p>
-                        </div>
-                        )
-                    })}
+                <h1>{this.state.user == null? "Hi!" : "Hi, " + this.state.user.username + "!"}</h1>
+                <div>
+                    <p>Your Points: {this.state.user == null? "Login to see your points" : this.state.user.points}</p>
+                    <p>Your Achievements:</p>
+                    <div class="wrapper">{this.state.achievements.map((ach, index) => {
+                        return (
+                            <div class="card">
+                                <img src={`${process.env.PUBLIC_URL}/${ach.icon_link}`} width="100px" height="100px" alt="Achievement image here"/>
+                                <h3>{ach.achievement_name}</h3>
+                                <p>{ach.achievement_description}</p>
+                            </div>
+                            )
+                        })}
+                    </div>
                 </div>
             </div>
-            <div>
-
-            </div>
-          </div>
         </div>
       );
     }
