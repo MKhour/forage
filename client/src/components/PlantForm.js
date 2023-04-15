7a9be08dd7a339dd799c37f91c7d91d882aae970
jackliu612 <jackliu612@gmail.com>
2023-04-15 12:07:01 -0400

    finish basic form->api connection

--- client/src/components/PlantForm.js
+++ client/src/components/PlantForm.js
@@ -1,49 +1,54 @@
 import { useState } from 'react';
 import Header from './Header';
+import './App.css';
 
 function PlantForm() {
-  const [name, setName] = useState("");
+  const [fileName, setFileName] = useState("");
+  const [result, setResult] = useState("");
+
+  const handleUpdate = (event) => {
+    const input = document.getElementById('pltFile');
+    setFileName(URL.createObjectURL(input.files[0]))
+    // alert(fileName);
+  }
 
   const handleSubmit = (event) => {
+    setResult("Loading...")
     event.preventDefault();
-    const input = document.getElementById('pltImg');
+    const input = document.getElementById('pltFile');
 
-    const body = new FormData
-    body.append("images", input.files[0]);
-    body.append("", "\\")
-    body.append("organs", "auto");
-    console.log(body);
-    alert('fetching');
+    const fd = new FormData
+    fd.append("images", input.files[0], input.files[0].name);
+    fd.append("organs", ["auto"]);
+    console.log(fd);
+    // alert('fetching');
     fetch("https://my-api.plantnet.org/v2/identify/all?include-related-images=false&no-reject=false&lang=en&api-key=2b10gZoCiPfqdoc0OQ9So4f6V", {
-    body,
-    headers: {
-        Accept: "application/json",
-        "Content-Type": "multipart/form-data"
-    },
+    body: fd,
     method: "POST"
     })
     .then((response) => response.json())
     .then((result) => {
-        const x = result;
         console.log(result);
-        alert(result);
+        setResult(result.bestMatch)
     });
 
-    alert(`The name you entered was: ${input.files[0].name}`)
+    // alert(`The name you entered was: ${input.files[0].name}`)
   }
 
   return (
     <div className="App">
         <Header />
-        <form onSubmit={handleSubmit}>
-        <label>Enter your name:
+        <form onSubmit={handleSubmit} onChange={handleUpdate}>
+        <label>Upload a plant picture:
             <input 
                 type="file"
-                id="pltImg"
+                id="pltFile"
             />
         </label>
         <input type="submit" />
         </form>
+        <img id='pltImg' src={fileName}/>
+        <h3>{result}</h3>
     </div>
   )
 }
